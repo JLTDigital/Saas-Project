54954af5dc1eb80ff311055121202b57c8b3d589
JLTDigital <mrjltaft@gmail.com>
2024-10-26 22:53:12 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Header from './sections/Header.jsx'
+import Hero from './sections/Hero.jsx'
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <main className='overflow-hidden'>
+      <Header />
+      <Hero />
+    </main>
+  )
 }
 
 export default App
